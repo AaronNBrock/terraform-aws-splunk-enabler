ca4c9def295661e1b1031d9863effa51f9d76e8e
Aaron N. Brock <AaronNBrock@gmail.com>
2020-04-23 17:24:56 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "managed_account" {
   role_arn     = "arn:aws:iam::299840366891:role/aws-sentinel"
   endpoint_url = "https://splunk.jbennettconsulting.com:443"
   hec_token    = "beda0b1f-d761-4ea9-a15a-86ec16f119fd"
-  enabled      = true
+  enabled      = false
 }
 
 module "managed_account2" {
@@ -16,5 +16,5 @@ module "managed_account2" {
   role_arn     = "arn:aws:iam::736998830959:role/terraform-aws-enable-splunk"
   endpoint_url = "https://splunk.jbennettconsulting.com:443"
   hec_token    = "beda0b1f-d761-4ea9-a15a-86ec16f119fd"
-  enabled      = true
+  enabled      = false
 }
