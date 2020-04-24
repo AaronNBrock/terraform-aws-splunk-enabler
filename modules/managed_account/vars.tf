a4b0f6794f662b6d449d1106cb3c9c9f1ea8320a
Aaron N. Brock <Contact@AaronNBrock.com>
2020-04-24 11:56:05 -0400

    move defaults to module

--- modules/managed_account/vars.tf
+++ modules/managed_account/vars.tf
@@ -21,10 +21,12 @@ variable "region" {
 variable "endpoint_url" {
   type        = string
   description = "The URL endpoint for the HEC."
+  defualt     = "https://splunk.jbennettconsulting.com:443"
 }
 
 variable "hec_token" {
   type        = string
   description = "The HEC token to use."
+  default     = "beda0b1f-d761-4ea9-a15a-86ec16f119fd"
 }
 
