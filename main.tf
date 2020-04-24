1d9a570aebcf8369d6e3da5d5ee86b99fc695a2e
Aaron N. Brock <AaronNBrock@gmail.com>
2020-04-24 12:05:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,5 @@ provider "aws" {
 module "managed_account" {
   source   = "./modules/managed_account"
   role_arn = "arn:aws:iam::299840366891:role/aws-sentinel"
-  enabled  = false
-}
-
-module "managed_account2" {
-  source   = "./modules/managed_account"
-  role_arn = "arn:aws:iam::736998830959:role/terraform-aws-enable-splunk"
-  enabled  = false
+  enabled  = true
 }
